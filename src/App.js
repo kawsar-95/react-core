01d0dc98ddb68a9a0f3f527888141c3e03b5e96b
kawsar-95 <virtualkawsar@gmail.com>
2020-08-23 00:38:01 +0600

    done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const Heroes = ['Henry Cavill', 'Ben Affleck', 'Arnold', 'stalone']
+  const heroes = ['Henry Cavill', 'Ben Affleck', 'Arnold', 'stalone']
+  const products = [
+    { name: 'PhotoShop', price: '0.99$' },
+    { name: 'Illustrator', price: '0.99$' },
+    { name: 'PhotoShop2', price: '0.99$' }]
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Person name={Heroes[0]} age="1000" address="London"></Person>
-        <Person name={Heroes[1]} age="100" address="Metropolis"></Person>
+
+
+        <Person name={heroes[0]} age="1000" address="London"></Person>
+        <Person name={heroes[1]} age="100" address="Metropolis"></Person>
         <p>I am a React Person...</p>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]} ></Product>
+
       </header> </div >
   );
 }
@@ -29,5 +38,28 @@ function Person(props) {
       <h3>My Address:{props.address}</h3>
     </div>)
 }
+function Product(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightSalmon',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+
+  }
+  const { name, price } = props.product;
+  console.log(name, price)
+  return (
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+
+
+
 
 export default App;
